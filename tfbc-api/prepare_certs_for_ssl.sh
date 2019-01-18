7e5530006c8dc71ee8089c086b8aada0d9b1adbf
Nagendra <r.nagendra@gmail.com>
2019-01-18 23:00:32 +0530

    moving to port 443 instead of 3000 and using self signed cert

---
+++ tfbc-api/prepare_certs_for_ssl.sh
@@ -0,0 +1,1 @@
+openssl req -nodes -new -x509 -keyout server.key -out server.cert -subj "/C=IN/ST=Telangana/L=Hyderabad/O=TFBC Org/OU=IT Department/CN=tfbc.com"
--- tfbc-api/server.js
+++ tfbc-api/server.js
@@ -1,6 +1,16 @@
+var fs = require('fs');
+var https = require('https');
 var app = require('./app');
-var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function() {
+
+
+var port = process.env.PORT || 443; 
+
+
+var server = https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(port, function () {
   console.log('Express server listening on port ' + port);
-});
+})
