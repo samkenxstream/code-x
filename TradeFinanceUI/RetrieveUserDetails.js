7dca129074dd37e4616f829796f96478788e5e1e
Vamshi Krishna Kancharla <Vamshi.K.Kancharla@gmail.com>
2019-02-04 18:31:54 +0530

    TradeFinanceWebClient: Automatic updation of User Details based on User Type ( Buyer, Seller, Bank )

---
+++ TradeFinanceUI/RetrieveUserDetails.js
@@ -0,0 +1,142 @@
+
+var RetrieveUserDetails_Module = (function () {
+
+    var bDebug = false;
+
+    /****************************************************************************************
+    Retrieve the User-Details Records As per Input User Type ( Buyer, Seller, Bank )
+    *****************************************************************************************/
+
+    function retrieveUserDetails_FromMongoDB(webServerPrefix, queryObject_Record, Client_Request, selectionBoxId, additionalBoxField_Id) {
+
+        var xmlhttp;
+        var httpRequestString = webServerPrefix;
+
+        xmlhttp = new XMLHttpRequest();
+        httpRequestString += "Client_Request=" + Client_Request;
+
+        var queryObject_Keys = queryObject_Record.keys();
+
+        for (var currentKey of queryObject_Keys) {
+
+            httpRequestString += "&"
+            httpRequestString += currentKey + "=" + queryObject_Record.get(currentKey);
+        }
+
+        xmlhttp.open("POST", httpRequestString, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xmlhttp.setRequestHeader("accept", "application/json");
+
+        // Wait for Async response and Handle it in web page
+
+        xmlhttp.onreadystatechange = function () {
+
+            if (this.status == 200) {
+
+                if (this.readyState == 4) {
+
+                    //Parse the JSON Response Object
+                    //responseObject = JSON.parse(this.response);
+
+                    if (bDebug == true) {
+
+                        alert(" Response Content: " + responseObject);
+                    }
+
+                    alert("Successfully Retrieved the User Details Records through API : " + Client_Request);
+
+                    //Parse the JSON Response Object
+
+                    var responseString = this.response;
+                    var userDetailRecords = responseString.split("\n");
+                    var numOfRecords = userDetailRecords.length - 2;
+                    responseSingleObject = JSON.parse(userDetailRecords[numOfRecords]);
+
+                    alert("Success Response for RetrieveUserDetails : Last UserDetails Record => " + responseSingleObject);
+
+                    fillTheDetailsInSelectionBox(userDetailRecords, selectionBoxId, additionalBoxField_Id);
+
+                } else {
+
+                    if (bDebug == true) {
+
+                        alert("Intermediate Success Response for RetrieveUserDetails call :=> Status : " + this.status + " readyState : " + this.readyState);
+                    }
+                }
+
+            } else {
+
+                alert("Failure to place RetrieveUserDetails call :=> Status : " + this.status + " readyState : " + this.readyState);
+            }
+
+        };
+
+        if (bDebug == true) {
+
+            alert("Retrieving the Registered User Details from MongoDB => httpRequest : " + httpRequestString);
+        }
+
+        xmlhttp.send();
+
+    }
+
+    /***********************************************************************************************************
+    Helper Methods : fillTheDetailsInSelectionBoxss
+    ************************************************************************************************************/
+
+    function fillTheDetailsInSelectionBox(userDetailRecords, selectionBoxId, additionalBoxField_Id) {
+
+        // For all the User Details of corresponding UserType
+
+        for (var i = 0; i < userDetailRecords.length; i++) {
+
+            responseSingleObject = JSON.parse(userDetailRecords[i]);
+
+            var selectionBox = document.getElementById(selectionBoxId);
+
+            // Log error if userName doesn't exist
+
+            if (bDebug == true) {
+
+                if (responseSingleObject.Name != undefined && responseSingleObject.Name != null) {
+                    alert("inappropriate Name in current record of UserDetails = " + responseSingleObject.Name);
+                }
+            }
+
+            var currentUserName = responseSingleObject.Name;
+            var regExpr = /%20/gi;
+            currentUserName = currentUserName.replace(regExpr, " ");
+
+            // Add Option with UserName to the Selection Box
+
+            var currentElementToBeAdded = document.createElement("option");
+            currentElementToBeAdded.text = currentUserName;
+
+            selectionBox.add(currentElementToBeAdded);
+
+            // Add additinoal Field if not Null Input
+
+            if (additionalBoxField_Id != null) {
+
+                var additionalFieldBox = document.getElementById(additionalBoxField_Id);
+                var currentIdElementToBeAdded = document.createElement("option");
+                currentIdElementToBeAdded.text = responseSingleObject.UserName;
+
+                additionalFieldBox.add(currentIdElementToBeAdded);
+
+            }
+        }
+    }
+
+    /****************************************************************************************
+        Reveal private methods 
+    *****************************************************************************************/
+
+    return {
+
+        retrieveUserDetailsRecords: retrieveUserDetails_FromMongoDB
+
+    };
+
+})();
+
